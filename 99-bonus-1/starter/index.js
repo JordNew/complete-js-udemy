39914f8c27d2c848f22d80c143adb86e97759467
ab_jnieuwe1 <j.nieuwenburg@hva.nl>
2020-07-24 18:00:08 +0200

    Laptop Store implemented: responding to URL parameters

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -8,7 +8,8 @@ const laptopData = JSON.parse(json);
 const server = http.createServer((req, res) => {
     
     const pathName = url.parse(req.url, true).pathname;
-    
+    const id = url.parse(req.url, true).query.id;
+
     if (pathName === '/products' || pathName === '/') {
         // As a HEADER to our response, we write '200' for ok, as well as the CONTENT TYPE of what we are about to send back
         res.writeHead(200, {'Content-type':'text/html'});
@@ -16,9 +17,9 @@ const server = http.createServer((req, res) => {
         // The RESPONSE itself
         res.end('This is the PRODUCTS page!');
 
-    } else if (pathName === '/laptop') {
+    } else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type':'text/html'});
-        res.end('This is the LAPTOP page!');
+        res.end(`This is the LAPTOP page for laptop ${id}`);
     } else {
         res.writeHead(404, {'Content-type':'text/html'});
         res.end('URL was not found on the server');
