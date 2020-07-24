ae6d07acb3dcc020878887db02907808f872dbc0
ab_jnieuwe1 <j.nieuwenburg@hva.nl>
2020-07-24 17:43:29 +0200

    Laptop Store implemented routing

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const http = require('http');
+const url =  require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
     
-    // Every time the server gets accessed, our response is:
+    const pathName = url.parse(req.url, true).pathname;
+    
+    if (pathName === '/products' || pathName === '/') {
+        // As a HEADER to our response, we write '200' for ok, as well as the CONTENT TYPE of what we are about to send back
+        res.writeHead(200, {'Content-type':'text/html'});
+        
+        // The RESPONSE itself
+        res.end('This is the PRODUCTS page!');
 
-    // 1. We log it to the console, just for feedback's sake (not necessary)
-    console.log('Someone accessed the server!');
-    
-    // 2. As a HEADER to our response, we write '200' for ok, as well as the CONTENT TYPE of what we are about to send back
-    res.writeHead(200, {'Content-type':'text/html'});
-    
-    // 3. The RESPONSE itself
-    res.end('This is the response!');
+    } else if (pathName === '/laptop') {
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.end('This is the LAPTOP page!');
+    } else {
+        res.writeHead(404, {'Content-type':'text/html'});
+        res.end('URL was not found on the server');
+    }
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
