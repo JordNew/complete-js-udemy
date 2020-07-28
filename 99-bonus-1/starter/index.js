8b8adbb5d79348711ddaec50ff9d0f16e171f815
ab_jnieuwe1 <j.nieuwenburg@hva.nl>
2020-07-28 11:48:21 +0200

    Laptop Store generate HTML (dynamically) based on data

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -10,16 +10,37 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
+    // PRODUCTS OVERVIEW
     if (pathName === '/products' || pathName === '/') {
         // As a HEADER to our response, we write '200' for ok, as well as the CONTENT TYPE of what we are about to send back
         res.writeHead(200, {'Content-type':'text/html'});
         
         // The RESPONSE itself
-        res.end('This is the PRODUCTS page!');
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
 
-    } else if (pathName === '/laptop' && id < laptopData.length) {
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                res.end(overviewOutput);
+            });
+        });  
+
+    // LAPTOP DETAIL
+    } else if (pathName === '/laptop' && id < laptopData.length && id >= 0) {
+        // HEADER
         res.writeHead(200, {'Content-type':'text/html'});
-        res.end(`This is the LAPTOP page for laptop ${id}`);
+        
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            // const type = typeof data;
+            // console.log('type of data = ' + type);
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        });
+
+    // URL NOT FOUND    
     } else {
         res.writeHead(404, {'Content-type':'text/html'});
         res.end('URL was not found on the server');
@@ -28,6 +49,18 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(1337, '127.0.0.1', () => {
-    console.log('Server is listening for requests');
+    console.log('Server is listening for requests ... ');
 });
 
+function replaceTemplate(originalHTML, laptop) {
+    let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
